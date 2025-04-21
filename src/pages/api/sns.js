640fbcde555f819975abcdfba0d05eb95e8bd5cc
Yazeed Altaweel <63609245+ya-zeed@users.noreply.github.com>
2025-04-21 11:20:48 +0300

    Update sns.js

--- src/pages/api/sns.js
+++ src/pages/api/sns.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+    console.log(req.method);
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method Not Allowed' });
     }
